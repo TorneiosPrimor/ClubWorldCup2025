1e0d78b8cac4bfb5cfe24dc578fca5fd88518538
TorneiosPrimor <g.araujoveroneze@gmail.com>
2025-03-15 18:37:34 -0400

    Atualizar o index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ const times = ['Inter Miami', 'Benfica', 'Porto', 'Al Ahly', 'Auckland City'];
 
 const resultados = [
     { data: '14/03', dia: 'Sex', hora: '19:00', time1: 'Porto', gols1: '1', artilheiros1: ['Ceará'], time2: 'Al Ahly', gols2: '9', artilheiros2: ['Macarrão', 'Macarrão', 'Macarrão', 'Elienderson', 'Poke', 'Xandão', 'Alex', 'Burguês', 'Burguês'] },
-    { data: '15/03', dia: 'Sáb', hora: '17:00', time1: 'Inter Miami', gols1: '', artilheiros1: [], time2: 'Benfica', gols2: '', artilheiros2: [] },
+    { data: '15/03', dia: 'Sáb', hora: '17:00', time1: 'Inter Miami', gols1: '3', artilheiros1: ['GC', 'João Miguel', 'Ewerton'], time2: 'Benfica', gols2: '1', artilheiros2: ['Helton'] },
 
     { data: '19/03', dia: 'Qua', hora: '19:00', time1: 'Inter Miami', gols1: '', artilheiros1: [], time2: 'Porto', gols2: '', artilheiros2: [] },
     { data: '21/03', dia: 'Sex', hora: '19:00', time1: 'Benfica', gols1: '', artilheiros1: [], time2: 'Auckland City', gols2: '', artilheiros2: [] },
@@ -33,7 +33,9 @@ const resultados = [
 ];
 
 const cartoes = [
-    { jogador: "Mario", time: "Al Ahly", amarelos: 1, vermelhos: 0 }
+    { jogador: "Mario", time: "Al Ahly", amarelos: 1, vermelhos: 0 },
+    { jogador: "Eduardo", time: "Inter Miami", amarelos: 1, vermelhos: 0 },
+    { jogador: "Hamilton", time: "Benfica", amarelos: 1, vermelhos: 0 }
 ];
 
 const eliminatorias = [
